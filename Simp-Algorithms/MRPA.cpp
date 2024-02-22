b9d6ea645485a0a3faa075a5d1aab597c7c400a2
KarmaKamikaze <nicolaijgn@gmail.com>
2024-02-22 11:06:25 +0100

    Create MRPA class

---
+++ Simp-Algorithms/MRPA.cpp
@@ -0,0 +1,8 @@
+//
+// Created by karma on 2/22/24.
+//
+
+#include "MRPA.h"
+
+namespace simp_algorithms {
+} // simp_algorithms
---
+++ Simp-Algorithms/MRPA.h
@@ -0,0 +1,16 @@
+//
+// Created by karma on 2/22/24.
+//
+
+#ifndef TRACE_Q_MRPA_H
+#define TRACE_Q_MRPA_H
+
+namespace simp_algorithms {
+
+    class MRPA {
+
+    };
+
+} // simp_algorithms
+
+#endif //TRACE_Q_MRPA_H
