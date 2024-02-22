589e18f950fae2ffd708deffe001812536aa060c
Kristoffer <33570390+Kristoffermg@users.noreply.github.com>
2024-02-22 21:27:49 +0100

    T-drive dataset storage + printing
    Stores the whole T-drive dataset in the data structure. Currently it stores the whole dataset in memory, could be changed to a database or batch approach instead

--- datastructure/trajectory.cpp
+++ datastructure/trajectory.cpp
@@ -1,33 +1,83 @@
 #include "trajectory.h"
 #include <iostream>
 #include <vector>
+#include <string>
+#include <filesystem>
+#include <fstream>
+
+using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
+
+// Constants
+const std::filesystem::path TDRIVE_PATH = std::filesystem::current_path().parent_path() / "datasets" / "t-drive";
+const char DELIMITER = ',';
 
 struct Location {
-    double longitude;
-    double latitude;
+    std::string timestamp{};
+    double longitude{};
+    double latitude{};
 };
 
 struct Trajectory {
-    int id;
-    std::vector<Location> locations;
+    int id{};
+    std::vector<Location> locations{};
 };
 
+//class AllTrajectories {
+//    public:
+//        std::vector<Trajectory> trajectories;
+//};
+//
+//AllTrajectories allTrajectories;
+
+std::vector<Trajectory> allTrajectories;
+
+void load_tdrive_dataset() {
+    for (const auto& dirEntry : recursive_directory_iterator(TDRIVE_PATH))  {
+        std::ifstream file(dirEntry.path());
+
+        if(file.is_open()) {
+            std::string line;
+
+            while(std::getline(file, line)) {
+                Location location;
+                Trajectory trajectory;
+
+                std::istringstream lineStream(line);
+                std::string id, timestamp, longitude, latitude;
+                if(std::getline(lineStream, id, DELIMITER) &&
+                        std::getline(lineStream, timestamp, DELIMITER) &&
+                        std::getline(lineStream, longitude, DELIMITER) &&
+                        std::getline(lineStream, latitude, DELIMITER)) {
+                    location.timestamp = timestamp;
+                    location.longitude = std::stod(longitude);
+                    location.latitude = std::stod(latitude);
+                    trajectory.id = std::stoi(id);
+                    trajectory.locations.push_back(location);
+                    allTrajectories.push_back(trajectory);
+                } else {
+                    std::cerr << "Error reading line: " << line << std::endl;
+                }
+            }
+            file.close();
+        } else {
+            throw std::runtime_error("Error opening file: " + dirEntry.path().string());
+        }
+    }
+}
+
+void print_trajectories() {
+    for (const auto & trajectory : allTrajectories) {
+        std::cout << "id: " << trajectory.id << std::endl;
+        for (const auto & location : trajectory.locations) {
+            std::cout << "timestamp: " << location.timestamp << std::endl;
+            std::cout << "longitude, latitude: " << location.longitude << " " << location.latitude << std::endl;
+        }
+    }
+}
+
 void trajectorytest () {
-    Location location1;
-    location1.longitude = 3.446;
-    location1.latitude = 53.345;
-
-    Location location2;
-    location2.longitude = 59.344;
-    location2.latitude = 21.433;
-
-    Trajectory trajectory1;
-    trajectory1.id = 1;
-    trajectory1.locations.push_back(location1);
-    trajectory1.locations.push_back(location2);
-
-
-    std::cout << "id: " << trajectory1.id << " long1, lat1: " << trajectory1.locations[0].longitude << " " << trajectory1.locations[0].latitude << std::endl;
+    load_tdrive_dataset();
+    print_trajectories();
 }
 
 
