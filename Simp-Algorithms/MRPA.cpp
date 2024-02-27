de739ce3272a904d9093132826ed77917f3fe444
KarmaKamikaze <nicolaijgn@gmail.com>
2024-02-27 12:59:14 +0100

    Begun work in MRPA error tolerance

--- Simp-Algorithms/MRPA.cpp
+++ Simp-Algorithms/MRPA.cpp
@@ -2,7 +2,32 @@
 // Created by karma on 2/22/24.
 //
 
+#include <cmath>
 #include "MRPA.h"
 
 namespace simp_algorithms {
+    std::vector<double> MRPA::error_tolerance_init(Trajectory const& traj) {
+        std::vector<double> result{};
+
+        auto number_of_tolerances = static_cast<int>(std::log(traj.points.size()) / std::log(resolution_scale));
+
+        for (auto k = 0; k < number_of_tolerances; k++) {
+            auto resolution = static_cast<double>(traj.points.size()) / std::pow(resolution_scale, k);
+            double error_tolerance{};
+
+            for (auto j = 1; j < resolution; j++) {
+                error_tolerance += LSSD();
+            }
+
+            error_tolerance *= 1 / (resolution - 1);
+            result.emplace_back(error_tolerance);
+        }
+
+        return result;
+    }
+
+    Trajectory MRPA::approximate_simplified_trajectory(Trajectory const& original_trajectory) {
+
+    }
+
 } // simp_algorithms
--- Simp-Algorithms/MRPA.h
+++ Simp-Algorithms/MRPA.h
@@ -5,10 +5,32 @@
 #ifndef TRACE_Q_MRPA_H
 #define TRACE_Q_MRPA_H
 
+#include <vector>
+
 namespace simp_algorithms {
 
-    class MRPA {
+    class Trajectory {
+    private:
+        struct Point {
+            double x {};
+            double y {};
+            double t {};
+        };
+    public:
+        std::vector<Point> points {};
+    };
 
+    class MRPA {
+        using Trajectory = simp_algorithms::Trajectory;
+    private:
+        double eps {};
+        double resolution_scale {2}; // c
+        double ISSD(Trajectory const&, Trajectory const&);
+        double LSSD(Trajectory const&, Trajectory const&);
+        double SED(Trajectory const&, Trajectory const&);
+
+        std::vector<double> error_tolerance_init(Trajectory const&);
+        Trajectory approximate_simplified_trajectory(Trajectory const&);
     };
 
 } // simp_algorithms
