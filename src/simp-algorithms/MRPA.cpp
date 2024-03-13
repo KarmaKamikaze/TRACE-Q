af64672eefbc557740de708758b112c69f72c336
KarmaKamikaze <nicolaijgn@gmail.com>
2024-03-13 11:22:52 +0100

    Refactor and document MRPA algorithm

--- src/simp-algorithms/MRPA.cpp
+++ src/simp-algorithms/MRPA.cpp
@@ -9,85 +9,6 @@
 #include "MRPA.hpp"
 
 namespace simp_algorithms {
-    std::vector<double> MRPA::error_tolerance_init(Trajectory const& trajectory) {
-        std::vector<double> result{};
-
-        auto number_of_tolerances = std::floor(std::log(trajectory.points.size()) / std::log(resolution_scale));
-
-        for (auto k = 1; k <= number_of_tolerances; k++) {
-            // Here we add 1 to the resolution and floor it in order to handle cases where the resolution calculation is exactly an int.
-            auto resolution = std::floor(static_cast<double>(trajectory.points.size()) / std::pow(resolution_scale, k) + 1);
-            double error_tolerance{};
-
-            for (auto j = 1; j < resolution; j++) {
-                auto range_start = static_cast<int>(std::floor((static_cast<double>(trajectory.points.size() - 1)) / (resolution - 1) * (j - 1) + 1));
-                auto range_end = static_cast<int>(std::floor((static_cast<double>(trajectory.points.size() - 1)) / (resolution - 1) * j + 1));
-
-                if (j == resolution - 1 && range_end < trajectory.points.size()) {
-                    throw std::range_error("Last range_end variable is not equal to size of trajectory");
-                }
-
-                error_tolerance += error_SED_sum(trajectory, range_start, range_end);
-            }
-
-            error_tolerance *= 1 / (resolution - 1);
-            result.emplace_back(error_tolerance);
-        }
-
-        return result;
-    }
-
-    Trajectory MRPA::approximated_temporally_synchronized_position(Trajectory const& trajectory, int i, int j) const {
-        Trajectory res {};
-
-        for (int k = i + 1; k < j; ++k) {
-            auto x = trajectory.points[i].x +
-                    ((trajectory.points[k].t - trajectory.points[i].t) /
-                    (trajectory.points[j].t - trajectory.points[i].t)) *
-                    (trajectory.points[j].x - trajectory.points[i].x);
-            auto y = trajectory.points[i].y +
-                    ((trajectory.points[k].t - trajectory.points[i].t) /
-                     (trajectory.points[j].t - trajectory.points[i].t)) *
-                     (trajectory.points[j].y - trajectory.points[i].y);
-            auto point = MRPA::Trajectory::Point(trajectory.points[k].order, x, y,trajectory.points[k].t);
-            res.points.emplace_back(point);
-        }
-
-        return res;
-    }
-
-    double MRPA::single_SED(Trajectory::Point const& original_point, Trajectory::Point const& approx_point) const
-    {
-        return sqrt(pow(original_point.x - approx_point.x, 2) + pow(original_point.y - approx_point.y, 2));
-    }
-
-    double MRPA::error_SED_sum(const simp_algorithms::MRPA::Trajectory& trajectory, int i, int j) {
-
-        // Here we subtract one from both i and j, due to 0-indexing
-        i = i - 1;
-        j = j - 1;
-        auto approx_trajectory = approximated_temporally_synchronized_position(trajectory, i, j);
-        double res {};
-        for (int k = i + 1; k < j; ++k) {
-            // Here we index the approx trajectory with the difference of k and i+1.
-            // This is because the approx trajectory does not have as many points,
-            // therefore it does not have values in the same indexes as the original trajectory.
-            res += pow(single_SED(trajectory.points[k], approx_trajectory.points[k - (i + 1)]), 2);
-        }
-
-        return res;
-    }
-
-    Trajectory MRPA::simplify(Trajectory const& trajectory, double const& query_error, double simplification_error) {
-
-        auto approximations = mrpa(trajectory);
-
-
-
-
-
-        return approximations.front();
-    }
 
     std::vector<Trajectory> MRPA::mrpa(Trajectory const& trajectory) {
         std::vector<Trajectory> result{};
@@ -106,19 +27,87 @@ namespace simp_algorithms {
         return result;
     }
 
-    data_structures::Node<Trajectory::Point> MRPA::apply_error_tolerance_scale_to_tree(Trajectory const& trajectory,
-                                                                                       int index,
-                                                                                       const std::vector<double>& error_tolerances) {
-        if (index == error_tolerances.size() - 1) {
-            auto scale = error_tolerances[index] / error_tolerances[index - 1];
-            return init_tree(trajectory, error_tolerances[index], error_tolerances[index] * scale);
+
+    std::vector<double> MRPA::error_tolerance_init(Trajectory const& trajectory) const {
+        std::vector<double> result{};
+
+        auto number_of_tolerances = std::floor(std::log(trajectory.points.size()) /
+                std::log(resolution_scale));
+
+        for (auto k = 1; k <= number_of_tolerances; k++) {
+            // Here we add 1 to the resolution and floor it in order to handle cases where the resolution
+            // calculation is exactly an int.
+            auto resolution = std::floor(static_cast<double>(trajectory.points.size()) /
+                    std::pow(resolution_scale, k) + 1);
+            double error_tolerance{};
+
+            for (auto j = 1; j < resolution; j++) {
+                auto range_start = static_cast<int>(std::floor((static_cast<double>(trajectory.points.size() - 1)) /
+                        (resolution - 1) * (j - 1) + 1));
+                auto range_end = static_cast<int>(std::floor((static_cast<double>(trajectory.points.size() - 1)) /
+                        (resolution - 1) * j + 1));
+
+                if (j == resolution - 1 && range_end < trajectory.points.size()) {
+                    throw std::range_error("Last range_end variable is not equal to size of trajectory");
+                }
+
+                error_tolerance += error_SED_sum(trajectory, range_start, range_end);
+            }
+
+            error_tolerance *= 1 / (resolution - 1);
+            result.emplace_back(error_tolerance);
         }
-        else {
-            return init_tree(trajectory, error_tolerances[index], error_tolerances[index + 1]);
-        }
+
+        return result;
     }
 
-    data_structures::Node<Trajectory::Point> MRPA::init_tree(Trajectory const& trajectory, double error_tol, double high_error_tol) {
+
+    double MRPA::error_SED_sum(const simp_algorithms::MRPA::Trajectory& trajectory, int i, int j) {
+
+        // Here we subtract one from both i and j, due to 0-indexing
+        i = i - 1;
+        j = j - 1;
+        auto approx_trajectory = approximated_temporally_synchronized_position(trajectory, i, j);
+        double res {};
+        for (int k = i + 1; k < j; ++k) {
+            // Here we index the approx trajectory with the difference of k and i+1.
+            // This is because the approx trajectory does not have as many points,
+            // therefore it does not have values in the same indexes as the original trajectory.
+            res += pow(single_SED(trajectory.points[k],
+                                  approx_trajectory.points[k - (i + 1)]), 2);
+        }
+
+        return res;
+    }
+
+
+    Trajectory MRPA::approximated_temporally_synchronized_position(Trajectory const& trajectory, int i, int j) {
+        Trajectory res {};
+
+        for (int k = i + 1; k < j; ++k) {
+            auto x = trajectory.points[i].x +
+                     ((trajectory.points[k].t - trajectory.points[i].t) /
+                      (trajectory.points[j].t - trajectory.points[i].t)) *
+                     (trajectory.points[j].x - trajectory.points[i].x);
+            auto y = trajectory.points[i].y +
+                     ((trajectory.points[k].t - trajectory.points[i].t) /
+                      (trajectory.points[j].t - trajectory.points[i].t)) *
+                     (trajectory.points[j].y - trajectory.points[i].y);
+            auto point = MRPA::Trajectory::Point(trajectory.points[k].order, x, y,trajectory.points[k].t);
+            res.points.emplace_back(point);
+        }
+
+        return res;
+    }
+
+
+    double MRPA::single_SED(Trajectory::Point const& original_point, Trajectory::Point const& approx_point) {
+        return sqrt(pow(original_point.x - approx_point.x, 2) + pow(original_point.y - approx_point.y, 2));
+    }
+
+
+    data_structures::Node<Trajectory::Point> MRPA::init_tree(Trajectory const& trajectory,
+                                                             double error_tol, double high_error_tol) {
 
         MRPA_PTQ working_list{compare};
         working_list.push(trajectory.points.front());
@@ -129,8 +118,8 @@ namespace simp_algorithms {
 
         while (!unvisited.empty()) {
             while (!working_list.empty()) {
-                maintain_priority_queue(root, trajectory, error_tol, high_error_tol, working_list, future_work,
-                                        unvisited);
+                maintain_priority_queue(root, trajectory, error_tol, high_error_tol, working_list,
+                                        future_work, unvisited);
             }
 
             while (!future_work.empty()) {
@@ -145,8 +134,9 @@ namespace simp_algorithms {
         return root;
     }
 
-    void MRPA::maintain_priority_queue(Node& tree, Trajectory const& trajectory, double error_tol, double high_error_tol,
-                                       MRPA_PTQ& working_list, MRPA_PTQ& future_work,
+
+    void MRPA::maintain_priority_queue(Node& tree, Trajectory const& trajectory, double error_tol,
+                                       double high_error_tol, MRPA_PTQ& working_list, MRPA_PTQ& future_work,
                                        std::vector<Trajectory::Point>& unvisited) {
         Trajectory::Point index1 = working_list.top();
         working_list.pop();
@@ -177,6 +167,7 @@ namespace simp_algorithms {
         // Returns through side effects in parameters
     }
 
+
     Trajectory MRPA::approximate(const Trajectory& trajectory, const Node& tree, double error_tol) {
 
         std::vector<double> approx_error(trajectory.points.size(), std::numeric_limits<double>::max());
@@ -197,7 +188,7 @@ namespace simp_algorithms {
                 for (const auto& node_index2: children){
                     double error = error_SED_sum(trajectory, node_index1.data.order, node_index2.data.order);
                     if ((approx_error[node_index1.data.order - 1] + error < approx_error[node_index2.data.order - 1])
-                    && error <= error_tol) {
+                        && error <= error_tol) {
                         backtrack[node_index2.data.order] = node_index1.data;
                         approx_error[node_index2.data.order - 1] = approx_error[node_index1.data.order - 1] + error;
                     }
@@ -217,12 +208,34 @@ namespace simp_algorithms {
         Trajectory result_trajectory{{std::begin(result), std::end(result)}};
 
         // Before returning we must update the order values of the nodes.
-        // This is because we use order to index, and since we return a trajectory with fewer points, we can index out of range!
+        // This is because we use order to index, and since we return a trajectory with fewer points,
+        // we can index out of range!
         for (int i = 0; i < result_trajectory.points.size(); ++i) {
             result_trajectory.points[i].order = i + 1;
         }
 
         return result_trajectory;
+    }
+
+
+    data_structures::Node<Trajectory::Point> MRPA::apply_error_tolerance_scale_to_tree(
+            Trajectory const& trajectory, int index, const std::vector<double>& error_tolerances) {
+
+        if (index == error_tolerances.size() - 1) {
+            auto scale = error_tolerances[index] / error_tolerances[index - 1];
+            return init_tree(trajectory, error_tolerances[index], error_tolerances[index] * scale);
+        }
+        else {
+            return init_tree(trajectory, error_tolerances[index], error_tolerances[index + 1]);
+        }
+    }
+
+
+    Trajectory MRPA::simplify(Trajectory const& trajectory, double const& query_error, double simplification_error) {
+
+        auto approximations = mrpa(trajectory);
+
+        return approximations.front();
     }
 
 } // simp_algorithms
--- src/simp-algorithms/MRPA.hpp
+++ src/simp-algorithms/MRPA.hpp
@@ -35,21 +35,26 @@ namespace simp_algorithms {
     class MRPA {
         using Trajectory = simp_algorithms::Trajectory;
         using Node = data_structures::Node<Trajectory::Point>;
-        static constexpr auto compare = [](Trajectory::Point const &left, Trajectory::Point const &right) {
+        static constexpr auto compare =
+                [](Trajectory::Point const &left, Trajectory::Point const &right) {
             return left.order < right.order;
         };
         using MRPA_PTQ = std::priority_queue<Trajectory::Point, std::vector<Trajectory::Point>, decltype(compare)>;
-    private:
-        double eps {};
-        int const resolution_scale {2}; // c
-        [[nodiscard]] Trajectory approximated_temporally_synchronized_position(Trajectory const &trajectory, int i, int j) const;
+
         /**
-         * Calculates the SED between an original point and its approximated temporal synchronized position.
-         * @param original_point The original trajectory point.
-         * @param approx_point The associated approximated trajectory point.
-         * @return The SED error (double).
+         * The resolution scale (called c in the MRPA paper) describes the number of the intermediate scale.
+         * The lower c is, the more error tolerances we will have; thereby we have more simplified trajectories
+         * to choose from. Additionally, the more error tolerances, the less the difference is between them.
          */
-        [[nodiscard]] double single_SED(Trajectory::Point const& original_point, Trajectory::Point const& approx_point) const;
+        double const resolution_scale {2}; // c
+
+        /**
+         * Calculates a vector of error tolerances for the given trajectory based on the resolution scale and the
+         * number of points in the trajectory, which describes the granularity of the resolution windows.
+         * @param trajectory The trajectory which for we calculate error tolerances.
+         * @return A vector of error tolerances for the given trajectory.
+         */
+        [[nodiscard]] std::vector<double> error_tolerance_init(Trajectory const& trajectory) const;
 
         /**
          * Calculates the sum of SED error over a range of an input trajectory.
@@ -58,17 +63,28 @@ namespace simp_algorithms {
          * @param j End of the range of points from the trajectory to determine the SED error sum for.
          * @return The SED error sum for the range of points in the trajectory.
          */
-        double error_SED_sum(const simp_algorithms::MRPA::Trajectory& trajectory, int i, int j);
-        Node apply_error_tolerance_scale_to_tree(Trajectory const& trajectory, int index,
-                                                 const std::vector<double>& error_tolerances);
-        void maintain_priority_queue(Node& tree, Trajectory const& trajectory, double error_tol, double high_error_tol,
-                                     MRPA_PTQ& working_list, MRPA_PTQ& future_work,
-                                     std::vector<Trajectory::Point>& unvisited);
-        std::vector<double> error_tolerance_init(Trajectory const& trajectory);
-        Trajectory approximate(const Trajectory& trajectory, const Node& tree, double error_tol);
+        static double error_SED_sum(const simp_algorithms::MRPA::Trajectory& trajectory, int i, int j);
+
+        /**
+         * Considers both spatial and temporal information on sub-trajectories to create approximate point.
+         * @param trajectory Trajectory under approximation.
+         * @param i Start of sub-trajectory considered for approximation.
+         * @param j End of sub-trajectory considered for approximation.
+         * @return The new approximation point.
+         */
+        static Trajectory approximated_temporally_synchronized_position(Trajectory const &trajectory, int i, int j);
+
+        /**
+         * Calculates the SED between an original point and its approximated temporal synchronized position.
+         * @param original_point The original trajectory point.
+         * @param approx_point The associated approximated trajectory point.
+         * @return The SED error (double).
+         */
+        static double single_SED(Trajectory::Point const& original_point, Trajectory::Point const& approx_point);
+
         /**
          * Initializes and returns a single root node, describing a tree structure.
-         * A node consists of a parent and children, wherein the children are vertices of higher order, 
+         * A node consists of a parent and children, wherein the children are vertices of higher order,
          * where the error SED sum of the trajectory points with range from the parent to the child comply
          * with the error tolerance.
          * @param trajectory The trajectory from which a tree structure must be made.
@@ -76,10 +92,48 @@ namespace simp_algorithms {
          * @param high_error_tol A high error tolerance, which is used to skip ahead when the error becomes too high.
          * @return The constructed tree.
          */
-        Node init_tree(Trajectory const& trajectory, double error_tol, double high_error_tol);
+        static Node init_tree(Trajectory const& trajectory, double error_tol, double high_error_tol);
+
+        /**
+         * Maintains the two priority queues working_list and future_work.
+         * Also removes elements from unvisited and updates the tree structure.
+         * @param tree The tree structure of the input trajectory.
+         * @param trajectory Trajectory to be simplified.
+         * @param error_tol The error tolerance that determines if a child is a child of a parent node.
+         * @param high_error_tol A high error tolerance, which is used to skip ahead when the error becomes too high.
+         * @param working_list The priority queue of vertices that are currently being processed.
+         * @param future_work The priority queue of vertices that will become the working_list in the next iteration.
+         * @param unvisited The set of vertices that have not yet been visited.
+         * Element are removed from this set, when they are added to future_work
+         */
+        static void maintain_priority_queue(Node& tree, Trajectory const& trajectory, double error_tol,
+                                            double high_error_tol, MRPA_PTQ& working_list, MRPA_PTQ& future_work,
+                                            std::vector<Trajectory::Point>& unvisited);
+
+        /**
+         *
+         * @param trajectory Trajectory to be simplified.
+         * @param tree Tree structure from init_tree.
+         * It describes the combinations of vertices that comply with the error tolerance.
+         * @param error_tol The error tolerance.
+         * @return The simplified trajectory
+         */
+        static Trajectory approximate(const Trajectory& trajectory, const Node& tree, double error_tol);
+
+        /**
+         * A helper function that ensures that init_tree is called with the correct high tolerance.
+         * @param trajectory Trajectory to be simplified.
+         * @param index The current index of the error_tolerances vector.
+         * @param error_tolerances The vector of increasing error tolerances
+         * @return The tree structure.
+         */
+        static Node apply_error_tolerance_scale_to_tree(Trajectory const& trajectory, int index,
+                                                        const std::vector<double>& error_tolerances);
+
     public:
 
-        Trajectory simplify(Trajectory const&, double const&, double); // Yeet
+        Trajectory simplify(Trajectory const&, double const&, double); // Yeet later https://tinyurl.com/tapvp9ch
+
         /**
          * Simplifies the input trajectory utilizing the MRPA algorithm.
          * @param trajectory The trajectory to be simplified.
