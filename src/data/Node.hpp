2c88eb5c5ff6e15c78a0343db2433c92b6da888e
KarmaKamikaze <nicolaijgn@gmail.com>
2024-03-13 14:19:17 +0100

    Document Node template

--- src/data/Node.hpp
+++ src/data/Node.hpp
@@ -16,30 +16,42 @@ namespace data_structures {
         std::vector<Node<T>> children {};
 
         explicit Node(T data) : data(data) {};
+
+        /**
+         * Finds a node with the specified data value.
+         * @param node_data The data value to search for.
+         * @return A reference to the found node or a dummy node if not found.
+         */
         Node<T>& find(const T& node_data) {
             static auto dummy = Node{T{}};
 
             if (this->data == node_data) {
-                return *this;
+                return *this; // Found the node
             }
 
             for (auto& child : children) {
                 auto& found_node = child.find(node_data);
                 if (found_node.data == node_data) {
-                    return found_node;
+                    return found_node; // Found the node in a child
                 }
             }
 
-            return dummy;
+            return dummy; // Data not found
         }
 
+        /**
+         * Overloads the << operator to print the node and its children.
+         * @param os The output stream.
+         * @param node The node to print.
+         * @return The modified output stream.
+         */
         friend std::ostream& operator<<(std::ostream& os, const Node& node) {
             os << node.data;
             if (!node.children.empty()) {
                 os << "{";
             }
             for (const auto& x : node.children) {
-                os << x << " ";
+                os << x << " "; // Recursively print child nodes
             }
             if (!node.children.empty()) {
                 os << "}";
