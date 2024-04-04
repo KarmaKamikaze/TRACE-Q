652327069a413df7a8f5f81c9518f4378e5c7210
KarmaKamikaze <nicolaijgn@gmail.com>
2024-04-04 10:28:44 +0200

    Fix indentation

--- src/querying/Range_Query_Test.cpp
+++ src/querying/Range_Query_Test.cpp
@@ -2,7 +2,7 @@
 #include "Range_Query_Test.hpp"
 
 namespace spatial_queries {
-        bool Range_Query_Test::operator()(data_structures::Trajectory const& trajectory) {
+    bool Range_Query_Test::operator()(data_structures::Trajectory const& trajectory) {
         return Range_Query::in_range(trajectory, window) == original_in_window;
     }
 
