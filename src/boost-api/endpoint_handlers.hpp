c80eb1f279bdc457975638135e6e09e4aa33d1c1
Kristoffer <33570390+Kristoffermg@users.noreply.github.com>
2024-04-08 14:37:42 +0200

    Removed unused hpp function

--- src/boost-api/endpoint_handlers.hpp
+++ src/boost-api/endpoint_handlers.hpp
@@ -7,8 +7,7 @@
 
 namespace api {
     using namespace boost::beast::http;
-
-    // Define a handler type for request handling functions
+    
     using RequestHandler = std::function<void(const request<string_body> &, response<string_body> &)>;
 
     void handle_insert_trajectories_into_trajectory_table(const request<string_body> &req, response<string_body> &res);
@@ -20,8 +19,6 @@ namespace api {
     void handle_reset_all_data(const request<string_body> &req, response<string_body> &res);
 
     void handle_root(const request<string_body> &req, response<string_body> &res);
-
-    void handle_hello(const request<string_body> &req, response<string_body> &res);
 
     void handle_not_found(const request<string_body> &req, response<string_body> &res);
 }
