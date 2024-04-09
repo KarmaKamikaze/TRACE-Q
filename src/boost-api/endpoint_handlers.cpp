96ba3c018531215a4ff2de3125b0132fa46e76f0
Martin Langgaard Jacobsen <martin.l.jacobsen2010@live.dk>
2024-04-09 09:33:26 +0200

    Something something, boost json is trash

--- src/boost-api/endpoint_handlers.cpp
+++ src/boost-api/endpoint_handlers.cpp
@@ -1,7 +1,7 @@
 #include "endpoint_handlers.hpp"
-#include <nlohmann/json.hpp>
-
-using json = nlohmann::json;
+#include "boost/json/src.hpp"
+#include "boost/json/stream_parser.hpp"
+#include "boost/json/monotonic_resource.hpp"
 
 namespace api {
     using namespace boost::beast::http;
@@ -14,115 +14,135 @@ namespace api {
         res.result(status::ok);
     }
 
-    json get_json_from_request_body(const request<string_body> &req,  response<string_body> &res) {
+    boost::json::value get_json_from_request_body(const request<string_body> &req, response<string_body> &res) {
         std::string requestBody = req.body();
 
-        json jsonData{};
+        boost::json::value jsonData;
         try {
-            jsonData = json::parse(requestBody);
+            boost::json::error_code ec;
+            jsonData = boost::json::parse(requestBody, ec);
+            if (ec)
+                throw std::runtime_error("Failed to parse JSON: " + ec.message());
         } catch (const std::exception& e) {
             res.result(status::bad_request);
             res.set(field::content_type, "text/plain");
             res.body() = "Failed to parse JSON: " + std::string(e.what());
+            return {};
         }
 
         return jsonData;
     }
 
     void handle_insert_trajectories_into_trajectory_table(const request<string_body> &req, response<string_body> &res) {
-        json jsonData = get_json_from_request_body(req, res);
-        if (jsonData.empty())
+        boost::json::value jsonData = get_json_from_request_body(req, res);
+        if (jsonData.is_null())
             return;
 
         std::vector<data_structures::Trajectory> all_trajectories{};
         trajectory_data_handling::db_table db_table{};
 
-        for (const auto& table : jsonData.items()) {
-            const auto &table_value = table.value();
-            db_table = table.key() == "original" ? trajectory_data_handling::db_table::original_trajectories : trajectory_data_handling::db_table::simplified_trajectories;
+        try {
+            auto jsonObject = jsonData.as_object();
+            for (auto it = jsonObject.begin(); it != jsonObject.end(); ++it) {
+                const auto &table_key = it->key();
+                const auto &table_value = it->value().as_object();
+                db_table = table_key == "original" ? trajectory_data_handling::db_table::original_trajectories
+                                                   : trajectory_data_handling::db_table::simplified_trajectories;
 
-            for(const auto &trajectory_iteration: table_value.items()) {
-                const auto &trajectoryData = trajectory_iteration.value();
+                for (auto jt = table_value.begin(); jt != table_value.end(); ++jt) {
+                    const auto &trajectory_id = jt->key();
+                    const auto &trajectoryData = jt->value().as_object();
 
-                data_structures::Trajectory trajectory;
-                trajectory.id = std::stoi(trajectory_iteration.key());
+                    data_structures::Trajectory trajectory;
+                    trajectory.id = std::stoi(trajectory_id);
 
-                for (const auto &location_iteration: trajectoryData.items()) {
-                    data_structures::Location location{};
-                    const auto &order = location_iteration.key();
-                    const auto &location_data = location_iteration.value();
+                    for (auto kt = trajectoryData.begin(); kt != trajectoryData.end(); ++kt) {
+                        const auto &location_order = kt->key();
+                        const auto &location_data = kt->value().as_object();
 
-                    location.order = std::stoi(order);
-                    location.timestamp = location_data["timestamp"];
-                    location.longitude = location_data["longitude"];
-                    location.latitude = location_data["latitude"];
+                        data_structures::Location location{};
+                        location.order = std::stoi(location_order);
+                        location.timestamp = location_data.at("timestamp").as_double();
+                        location.longitude = location_data.at("longitude").as_double();
+                        location.latitude = location_data.at("latitude").as_double();
 
-                    trajectory.locations.emplace_back(location);
+                        trajectory.locations.emplace_back(std::move(location));
+                    }
+                    all_trajectories.emplace_back(std::move(trajectory));
                 }
-                all_trajectories.emplace_back(trajectory);
             }
+            trajectory_manager::insert_trajectories_into_trajectory_table(all_trajectories, db_table);
+        } catch (const std::exception &e) {
+            res.result(status::bad_request);
+            res.set(field::content_type, "text/plain");
+            res.body() = "Error processing JSON data: " + std::string(e.what());
         }
-        trajectory_manager::insert_trajectories_into_trajectory_table(all_trajectories, db_table);
     }
 
     void handle_load_trajectories_into_rtree(const request<string_body> &req, response<string_body> &res) {
-        json jsonData = get_json_from_request_body(req, res);
-        if (jsonData.empty())
+        boost::json::value jsonData = get_json_from_request_body(req, res);
+        if (jsonData.is_null())
             return;
 
-        std::string db_table;
         try {
-            db_table = jsonData.at("db_table").get<std::string>();
+            std::string db_table = jsonData.as_object().at("db_table").as_string().c_str();
+
+            trajectory_data_handling::query_purpose purpose = (db_table == "original") ?
+                                                              trajectory_data_handling::query_purpose::insert_into_original_rtree_table :
+                                                              trajectory_data_handling::query_purpose::insert_into_simplified_rtree_table;
+
+            trajectory_data_handling::trajectory_manager::load_trajectories_into_rtree(purpose);
         } catch (const std::exception& e) {
             res.result(status::bad_request);
             res.set(field::content_type, "text/plain");
             res.body() = "Failed to read 'db_table' value from JSON: " + std::string(e.what());
-            return;
         }
-
-        trajectory_data_handling::query_purpose purpose = db_table == "original" ? trajectory_data_handling::query_purpose::insert_into_original_rtree_table : trajectory_data_handling::query_purpose::insert_into_simplified_rtree_table;
-
-        trajectory_data_handling::trajectory_manager::load_trajectories_into_rtree(purpose);
     }
 
     void handle_spatial_range_query_on_rtree_table(const request<string_body> &req, response<string_body> &res) {
-        json jsonData = get_json_from_request_body(req, res);
-        if (jsonData.empty())
+        boost::json::value jsonData = get_json_from_request_body(req, res);
+        if (jsonData.is_null())
             return;
 
-        trajectory_data_handling::query_purpose purpose;
-        if (jsonData.contains("original")) {
-            purpose = trajectory_data_handling::query_purpose::load_original_rtree_into_datastructure;
-        } else if (jsonData.contains("simplified")) {
-            purpose = trajectory_data_handling::query_purpose::load_simplified_rtree_into_datastructure;
-        } else {
+        try {
+            trajectory_data_handling::query_purpose purpose;
+
+            if (jsonData.as_object().contains("original")) {
+                purpose = trajectory_data_handling::query_purpose::load_original_rtree_into_datastructure;
+            } else if (jsonData.as_object().contains("simplified")) {
+                purpose = trajectory_data_handling::query_purpose::load_simplified_rtree_into_datastructure;
+            } else {
+                res.result(status::bad_request);
+                res.set(field::content_type, "text/plain");
+                res.body() = "Invalid JSON format: missing 'original' or 'simplified' key";
+                return;
+            }
+
+            boost::json::object originalData = jsonData.as_object().at("original").as_object();
+
+            double x_low = originalData.at("longitudeRange").as_object().at("min").as_double();
+            double x_high = originalData.at("longitudeRange").as_object().at("max").as_double();
+
+            double y_low = originalData.at("latitudeRange").as_object().at("min").as_double();
+            double y_high = originalData.at("latitudeRange").as_object().at("max").as_double();
+
+            long double t_low = originalData.at("timestampRange").as_object().at("min").as_double();
+            long double t_high = originalData.at("timestampRange").as_object().at("max").as_double();
+
+            spatial_queries::Range_Query::Window window{};
+            window.x_low = x_low;
+            window.x_high = x_high;
+            window.y_low = y_low;
+            window.y_high = y_high;
+            window.t_low = t_low;
+            window.t_high = t_high;
+
+            trajectory_data_handling::trajectory_manager::spatial_range_query_on_rtree_table(purpose, window);
+        } catch (const std::exception &e) {
             res.result(status::bad_request);
             res.set(field::content_type, "text/plain");
-            res.body() = "Invalid JSON format: missing 'original' or 'simplified' key";
-            return;
+            res.body() = "Error processing JSON data: " + std::string(e.what());
         }
-
-        auto& longitudeRange = jsonData["original"]["longitudeRange"];
-        double x_low = longitudeRange["min"];
-        double x_high = longitudeRange["max"];
-
-        auto& latitudeRange = jsonData["original"]["latitudeRange"];
-        double y_low = latitudeRange["min"];
-        double y_high = latitudeRange["max"];
-
-        auto& timestampRange = jsonData["original"]["timestampRange"];
-        long double t_low = timestampRange["min"];
-        long double t_high = timestampRange["max"];
-
-        spatial_queries::Range_Query::Window window{};
-        window.x_low = x_low;
-        window.x_high = x_high;
-        window.y_low = y_low;
-        window.y_high = y_high;
-        window.t_low = t_low;
-        window.t_high = t_high;
-
-        trajectory_data_handling::trajectory_manager::spatial_range_query_on_rtree_table(purpose, window);
     }
 
     void handle_reset_all_data(const request<string_body> &req, response<string_body> &res) {
