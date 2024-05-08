e277c989e770b8e2ded6d0108b009bfbf0f0499e
KarmaKamikaze <nicolaijgn@gmail.com>
2024-05-08 13:06:04 +0200

    Add command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,24 @@
 #include "TRACE_Q.hpp"
 #include "Start_API.hpp"
 #include "Endpoint_Handlers.hpp"
+#include "benchmark/TRACE_Q_Benchmark.hpp"
 
 int main(int argc, char* argv[]) {
     
     for(int i = 1; i < argc; i++) {
-        if(argv[i] == std::string{"--reset"}) {
+        if (argv[i] == std::string{"--reset"}) {
             trajectory_data_handling::Trajectory_Manager::reset_all_data();
+        }
+        if (argv[i] == std::string("--tests")) {
+            analytics::TRACE_Q_Benchmark::run_traceq_benchmarks();
+            return 0;
         }
     }
 
     // TRACE-Q test
     trajectory_data_handling::Trajectory_Manager::reset_all_data();
-    //trajectory_data_handling::File_Manager::load_tdrive_dataset();
-    trajectory_data_handling::File_Manager::load_geolife_dataset();
+    trajectory_data_handling::File_Manager::load_tdrive_dataset();
+    //trajectory_data_handling::File_Manager::load_geolife_dataset();
 
     //auto trace_q = trace_q::TRACE_Q{1.3, 0.98, 0.98, 8, 50, 0.05, 0.05, 3, 1.3, 0.05, 0.05, 3, true};
     //auto trace_q_no_knn = trace_q::TRACE_Q{2, 0.95, 8, 50, 0.05, 4, 1.3, 0.05, false};
